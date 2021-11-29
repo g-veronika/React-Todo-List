0552f1bcd708a8691920630b83371118c7fbd1aa
g-veronika <g.veronika2021@gmail.com>
2021-11-29 15:52:35 +0100

    remove activity from list

--- my-list/src/components/TodoList.js
+++ my-list/src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 
@@ -6,10 +7,45 @@ function TodoList() {
 
     const [todos, setTodos] = useState([]);
 
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+            return; 
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+    };
+
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    }
+
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete 
+            }
+
+            return todo;
+        })
+
+        setTodos(updatedTodos);
+    }
+
     return (
         <div>
             <h1>What's the Plan for Today?</h1>
-            <TodoForm />
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+            todos={todos} 
+            completeTodo={completeTodo} 
+            removeTodo={removeTodo} 
+            />
         </div>
     )
 }
