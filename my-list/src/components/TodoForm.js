97f4c753fe334eee77c13114691ff106f7bf9fd1
g-veronika <g.veronika2021@gmail.com>
2021-11-29 16:23:46 +0100

    styling added

--- my-list/src/components/TodoForm.js
+++ my-list/src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null);
 
@@ -25,16 +25,37 @@ function TodoForm(props) {
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+            <>    
             <input 
             type="text" 
-            placeholder="Add a todo" 
+            placeholder="Update your item" 
             value={input} 
             name="text" 
-            className="todo-input"
+            className="todo-input edit"
             onChange={handleChange} 
             ref={inputRef}
             />
-            <button className="todo-button">Add todo</button>
+            <button className="todo-button edit">Update</button>
+            
+            </>
+            ) : 
+            ( 
+            <>
+            <input 
+                type="text" 
+                placeholder="Add a todo" 
+                value={input} 
+                name="text" 
+                className="todo-input"
+                onChange={handleChange} 
+                ref={inputRef}
+                />
+                <button className="todo-button">Add todo</button>
+                </>
+                )}
+
+            
         </form>
     )
 }
